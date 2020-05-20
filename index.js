f66c6d7c5b36d9c44f637cd415b964aae6209cf9
henrychu04 <henrychu04@outlook.com>
2020-05-20 17:54:18 -0400

    discord bot format

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
-const inquirer = require('inquirer');
 const fetch = require('node-fetch');
 const AbortController = require('abort-controller');
+const Discord = require('discord.js');
+const client = new Discord.Client();
 
 const headers = {
     headers: {
         "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4136"
     }
 };
+
+client.once('ready', () => {
+	console.log('Ready!');
+});
+
+client.login('NzEyNzcxOTA3NzE5NTI4NDc4.XsWaoQ.0AhgP89tklcJroKhxtt998pqWkc');
 
 let controller = new AbortController();
 setTimeout(() => controller.abort(), 1000);
@@ -28,45 +35,32 @@ function timeoutPromise(ms, promise) {
     })
 }
 
-async function main() {
+client.on('message', async message => {
     try {
-        let url;
+        if (message.content.substring(0, 8) === '!shopify') {
+            
+            let url = message.content.slice(9);
+            
+            await timeoutPromise(1000, fetch(`${url}.json`, headers))
+                .then(response => response.json())
+                .then(data => {
+                    try {
+                        var vars = "";
 
-        await inquirer.prompt([
-            {
-                name: 'url',
-                message: 'Enter Shopify url:'
-            }])
-        .then(answer => {
-            url = answer.url;
-        });
-        
-        timeoutPromise(1000, fetch(`${url}.json`, headers))
-            .then(response => response.json())
-            .then(data => {
-                try {
-                    console.log();
-                    for (let i = 0; i < data['product']['variants'].length; i++) {
-                        console.log(`${data['product']['variants'][i]['title']} - ${data['product']['variants'][i]['id']}`);
-                    }
-                } catch (err) {
-                    if (err.message.includes('Cannot read property') && err.message.includes('of undefined')) {
-                        throw new Error('undefined');
-                    } else {
+                        for (var i = 0; i < data['product']['variants'].length; i++) {
+                            vars += `${data['product']['variants'][i]['title']} - ${data['product']['variants'][i]['id']}\n`;
+                        }
+
+                        message.channel.send(vars);
+                    } catch (err) {
                         throw err;
                     }
-                }
-            })
-            .catch(err => {
-                if (err.message == 'timeout' || err.message == 'undefined') {
-                    console.log('Site must be Shopify');
-                } else {
-                    throw err;
-                }
-            });        
+                })
+                .catch(() => {
+                    message.channel.send('Site must be Shopify');
+                });
+        }
     } catch (err) {
-        throw err;
+        message.channel.send('Unexpected error');
     }
-}
-
-main();
+});
