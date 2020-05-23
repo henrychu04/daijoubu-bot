07ed9bb84fee68bb15996b162abf75db07f77cc8
henrychu04 <henrychu04@outlook.com>
2020-05-23 02:28:25 -0400

    added domain name and edited size scraping

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const fetch = require('node-fetch');
 const AbortController = require('abort-controller');
 const Discord = require('discord.js');
+const url = require('url');
 const client = new Discord.Client();
 
 const headers = {
@@ -15,33 +16,36 @@ client.once('ready', () => {
 	console.log('Ready!');
 });
 
-let controller = new AbortController();
-setTimeout(() => controller.abort(), 1000);
-
-function timeoutPromise(ms, promise) {
-    return new Promise((resolve, reject) => {
-        const timeoutId = setTimeout(() => {
-            reject(new Error('timeout'))
-        }, ms);
-        promise.then(
-            (res) => {
-            clearTimeout(timeoutId);
-            resolve(res);
-        },
-        (err) => {
-            clearTimeout(timeoutId);
-            reject(err);
-        })
-    })
-}
-
 client.on('message', async message => {
     try {
         if (message.content.substring(0, 8) === '!shopify') {
+
+            console.log(`Message is ${message}`);
+
+            let controller = new AbortController();
+            setTimeout(() => controller.abort(), 1000);
+
+            function timeoutPromise(ms, promise) {
+                return new Promise((resolve, reject) => {
+                    const timeoutId = setTimeout(() => {
+                        reject(new Error('timeout'))
+                    }, ms);
+                    promise.then(
+                        (res) => {
+                        clearTimeout(timeoutId);
+                        resolve(res);
+                    },
+                    (err) => {
+                        clearTimeout(timeoutId);
+                        reject(err);
+                    })
+                })
+            }
             
-            let url = message.content.slice(9);
+            let link = message.content.slice(9);
+            let domain = url.parse(link).host;
             
-            await timeoutPromise(1000, fetch(`${url}.json`, headers))
+            await timeoutPromise(1000, fetch(`${link}.json`, headers))
                 .then(response => response.json())
                 .then(data => {
                     try {
@@ -49,12 +53,18 @@ client.on('message', async message => {
                             "embeds": [
                                 {
                                     "title": data['product']['title'],
-                                    "url": url,
+                                    "url": link,
                                     "color": 16777214,
                                     "fields": [
                                         {
                                             "name": "Price",
-                                            "value": "$" + data['product']['variants'][0]['price']
+                                            "value": "$" + data['product']['variants'][0]['price'],
+                                            "inline": true
+                                        },
+                                        {
+                                            "name": "Site",
+                                            "value": domain,
+                                            "inline": true
                                         },
                                         {
                                             "name": "Variants",
@@ -68,8 +78,17 @@ client.on('message', async message => {
                             ]
                         };
 
-                        for (var i = 0; i < data['product']['variants'].length; i++) {
-                            vars['embeds'][0]['fields'][1]['value'] += `${data['product']['variants'][i]['title']} - ${data['product']['variants'][i]['id']}\n`;
+                        var sizes = [];
+
+                        for (var i = 0; i < data['product']['options'].length; i++) {
+                            if (data['product']['options'][i]['name'] === 'Size') {
+                                sizes = data['product']['options'][i]['values']
+                                break;
+                            }
+                        }
+
+                        for (var i = 0; i < sizes.length; i++) {
+                            vars['embeds'][0]['fields'][2]['value'] += `${sizes[i]} - ${data['product']['variants'][i]['id']}\n`;
                         }
 
                         var api = process.env.SHOPIFY_VARIANTS_API;
@@ -94,7 +113,8 @@ client.on('message', async message => {
                     } else if (err.message === 'timeout') {
                         message.channel.send('```' + 'Site must be Shopify' + '```');
                     }
-                });
+                }
+            );
         }
     } catch (err) {
         console.log(err);
