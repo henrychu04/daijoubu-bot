cba8bd0436fa42bbb4d80660ed8d25aa9bc6816f
henrychu04 <henrychu04@outlook.com>
2020-06-04 04:41:47 -0400

    added supreme drop list and edits

--- README.md
+++ README.md
@@ -5,5 +5,6 @@ A Discord bot able to:
 - Scrape variants of a shopify product
 - Caclulate fees from different marketplaces: StockX, Goat, Stadium Goods
 - Calculates the optimal monitor delay for Shopify sites
+- Sends the latest Supreme Drop List
 
 Hosted on Heroku
--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const fetch = require('node-fetch');
 const AbortController = require('abort-controller');
 const url = require('url');
 const Money = require('js-money');
+const cheerio = require('cheerio');
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const api = process.env.BOT_TOOLS;
@@ -50,9 +51,9 @@ client.on('message', async (message) => {
 
       await timeoutPromise(1000, fetch(`${link}.json`, headers))
         .then((response) => response.json())
-        .then((data) => {
+        .then(async (data) => {
           try {
-            var vars = {
+            let vars = {
               username: 'Shopify Variants',
               embeds: [
                 {
@@ -82,28 +83,22 @@ client.on('message', async (message) => {
               ],
             };
 
-            var sizes = [];
+            let sizes = [];
 
-            for (var i = 0; i < data['product']['options'].length; i++) {
+            for (let i = 0; i < data['product']['options'].length; i++) {
               if (data['product']['options'][i]['name'] === 'Size') {
                 sizes = data['product']['options'][i]['values'];
                 break;
               }
             }
 
-            for (var i = 0; i < sizes.length; i++) {
+            for (let i = 0; i < sizes.length; i++) {
               vars['embeds'][0]['fields'][2][
                 'value'
               ] += `${sizes[i]} - ${data['product']['variants'][i]['id']}\n`;
             }
 
-            fetch(api, {
-              method: 'POST',
-              headers: {
-                'Content-Type': 'application/json',
-              },
-              body: JSON.stringify(vars),
-            }).then(console.log('Command completed'));
+            await sendWebhook(vars).then(console.log(`${message} completed`));
           } catch (err) {
             console.log(err);
             throw err;
@@ -123,24 +118,24 @@ client.on('message', async (message) => {
     if (message.content.substring(0, 4) === '!fee') {
       console.log(`Command: ${message}`);
 
-      var num = Money.fromDecimal(parseInt(message.content.slice(5)), 'USD');
+      let num = Money.fromDecimal(parseInt(message.content.slice(5)), 'USD');
 
-      var StockXFee1 = num.multiply(0.09, Math.ceil);
-      var StockXFee2 = num.multiply(0.03, Math.ceil);
+      let StockXFee1 = num.multiply(0.09, Math.ceil);
+      let StockXFee2 = num.multiply(0.03, Math.ceil);
       StockXFee1 = StockXFee1.add(StockXFee2);
-      var StockXRevenue = num.subtract(StockXFee1, Math.ceil);
+      let StockXRevenue = num.subtract(StockXFee1, Math.ceil);
 
-      var GoatFee1 = num.multiply(0.095, Math.ceil);
+      let GoatFee1 = num.multiply(0.095, Math.ceil);
       GoatFee1 = GoatFee1.add(new Money(500, Money.USD));
-      var GoatFee2 = num.subtract(GoatFee1, Math.ceil);
+      let GoatFee2 = num.subtract(GoatFee1, Math.ceil);
       GoatFee2 = GoatFee2.multiply(0.029, Math.ceil);
       GoatFee1 = GoatFee1.add(GoatFee2, Math.ceil);
-      var GoatRevenue = num.subtract(GoatFee1, Math.ceil);
+      let GoatRevenue = num.subtract(GoatFee1, Math.ceil);
 
-      var SGFee = num.multiply(0.2, Math.ceil);
-      var SGRevenue = num.subtract(SGFee, Math.ceil);
+      let SGFee = num.multiply(0.2, Math.ceil);
+      let SGRevenue = num.subtract(SGFee, Math.ceil);
 
-      var fee = {
+      let fee = {
         username: 'Fee Calculator',
         embeds: [
           {
@@ -197,24 +192,18 @@ client.on('message', async (message) => {
         ],
       };
 
-      fetch(api, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(fee),
-      }).then(console.log('Command completed'));
+      await sendWebhook(fee).then(console.log(`${message} completed`));
     }
 
     if (message.content.substring(0, 6) === '!delay') {
       try {
         console.log(`Command: ${message}`);
 
-        var command = message.content.slice(7);
-        var command_split = command.split(/\s+/);
+        let command = message.content.slice(7);
+        let command_split = command.split(/\s+/);
 
-        var task_num = command_split[0];
-        var proxy_num = command_split[1];
+        let task_num = command_split[0];
+        let proxy_num = command_split[1];
 
         if (
           task_num.length != 0 &&
@@ -222,15 +211,22 @@ client.on('message', async (message) => {
           !isNaN(task_num) &&
           !isNaN(proxy_num)
         ) {
-          var delay = Math.round((task_num * 3600) / proxy_num);
+          let delay = Math.round((task_num * 3600) / proxy_num);
 
-          message.channel.send(
-            '```' +
-              `Suggested delay for ${task_num} tasks and ${proxy_num} proxies: ${delay}` +
-              '```'
+          let delayEmbed = {
+            username: 'Delay Calculator',
+            embeds: [
+              {
+                title: `Suggested delay for ${task_num} tasks and ${proxy_num} proxies:`,
+                color: 16777214,
+                description: delay,
+              },
+            ],
+          };
+
+          await sendWebhook(delayEmbed).then(
+            console.log(`${message} completed`)
           );
-
-          console.log('Command completed');
         } else {
           throw new err();
         }
@@ -244,10 +240,117 @@ client.on('message', async (message) => {
       }
     }
 
+    if (message.content.substring(0, 9) === '!droplist') {
+      console.log(`Command: ${message}`);
+
+      const domain = 'https://www.supremecommunity.com';
+
+      let droplistPage = await fetch(
+        domain + '/season/spring-summer2020/droplists/',
+        { headers: headers }
+      )
+        .then((res) => {
+          return res.text();
+        })
+        .catch((err) => console.log(err));
+
+      const $1 = cheerio.load(droplistPage);
+
+      const latestURL = $1(
+        'div[class="col-xs-12 col-sm-12 col-md-10 box-list scapp-main-cont"]'
+      )
+        .find('div[class="col-sm-4 col-xs-12 app-lr-pad-2"]')
+        .find('a')
+        .attr('href');
+
+      let doc = await fetch(domain + latestURL, { headers: headers })
+        .then((res) => {
+          return res.text();
+        })
+        .catch((err) => console.log(err));
+
+      const $ = cheerio.load(doc);
+
+      const pageItems = $('div[class="card card-2"]');
+
+      if (pageItems.length > 0) {
+        let items = [];
+
+        pageItems.each((i, el) => {
+          let prop = {};
+
+          prop.name = $(el).find('h2').text();
+
+          prop.price = $(el).find('.label-price').text().trim();
+
+          const imageURL = $(el).find('img').attr('src');
+          prop.image = 'https://www.supremecommunity.com' + imageURL;
+
+          const description = $(el).find('img').attr('alt');
+          prop.description = description.split(' - ')[1];
+
+          const category = $(el).find('.category').text();
+          prop.category = category;
+
+          items.push(prop);
+        });
+
+        for (let crnt of items) {
+          let droplist = {
+            username: 'Latest Supreme Droplist',
+            embeds: [
+              {
+                title: crnt.name,
+                color: 16711680,
+                fields: [
+                  {
+                    name: 'Description',
+                    value: crnt.description,
+                  },
+                  {
+                    name: 'Price',
+                    value: crnt.price,
+                    inline: true,
+                  },
+                  {
+                    name: 'Category',
+                    value: crnt.category,
+                    inline: true,
+                  },
+                ],
+                thumbnail: {
+                  url: crnt.image,
+                },
+              },
+            ],
+          };
+
+          await sendWebhook(droplist);
+
+          await sleep(1000);
+        }
+      } else {
+        let droplist = {
+          username: 'Latest Supreme Droplist',
+          embeds: [
+            {
+              title: 'Droplist not out yet',
+              color: 16711680,
+              description: 'Stay tuned!',
+            },
+          ],
+        };
+
+        await sendWebhook(droplist);
+      }
+
+      console.log(`${message} completed`);
+    }
+
     if (message.content === '!help') {
       console.log(`Command: ${message}`);
 
-      var help = {
+      let help = {
         username: 'Commands',
         embeds: [
           {
@@ -266,18 +369,16 @@ client.on('message', async (message) => {
                 name: '!delay <number of tasks> <number of proxies>',
                 value: 'Delay Calculator based on 3600 delay',
               },
+              {
+                name: '!droplist',
+                value: 'Sends latest Supreme drop info',
+              },
             ],
           },
         ],
       };
 
-      fetch(api, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(help),
-      }).then(console.log('Command completed'));
+      await sendWebhook(help).then(console.log(`${message} completed`));
     }
   } catch (err) {
     console.log(err);
@@ -285,3 +386,21 @@ client.on('message', async (message) => {
       message.channel.send('```' + 'Unexpected error' + '```');
   }
 });
+
+function sleep(ms) {
+  return new Promise((resolve) => {
+    setTimeout(resolve, ms);
+  });
+}
+
+async function sendWebhook(embeded) {
+  await fetch(api, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(embeded),
+  }).catch((err) => {
+    console.log(err);
+  });
+}
