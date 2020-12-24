dbd5bc6a4f7d9a05a9a4cf0a9921ca74c7d30a8d
henrychu04 <henrychu04@outlook.com>
2020-12-24 14:16:42 -0500

    updated ping

--- commands/ping.js
+++ commands/ping.js
@@ -1,6 +1,8 @@
 exports.run = (client, message, args) => {
   message.channel
-    .send('pong!')
-    .then(console.log(`${message} completed\n`))
+    .send('Pinging ...')
+    .then((sent) => {
+      sent.edit(`Pong! Took ${sent.createdTimestamp - message.createdTimestamp}ms`);
+    })
     .catch(console.error);
 };
